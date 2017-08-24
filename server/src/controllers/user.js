b7a9a440dbd845999fc6a212f5d2a633b945af90
Femi Oladeji <femidotexe@gmail.com>
2017-08-24 14:57:00 +0100

    Implement jwt

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,7 +1,11 @@
-import { userModel } from '../schema';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+import { userModel } from '../schema';
 import { respond } from './utils';
 const saltRounds = 10;
+dotenv.config();
 
 class UserController {
     /**
@@ -21,7 +25,8 @@ class UserController {
             }
             bcrypt.compare(req.body.password, user.password, (err, valid) => {
                 if(valid) {
-                    return respond(res, 200, 'User logged in');
+                    const token = jwt.sign(user._id, process.env.SECRET, { expiresIn: '1d'});
+                    return respond(res, 200, token);
                 } else {
                     return respond(res, 401, 'Invalid credentials');
                 }
--- server/src/controllers/utils.js
+++ server/src/controllers/utils.js
@@ -1,4 +1,4 @@
-export const respond = (res, statusCode, message) => {
+export const respond = (res, statusCode, payload) => {
     const status = (statusCode === 200 || statusCode === 201) ? true : false;
-    return res.status(statusCode).json({ status, message });
+    return res.status(statusCode).json({ status, payload });
 }
